5bde06050c4ada915b0ed386e584662df00468bf
whembree87 <whembree87@gmail.com>
2018-11-26 16:29:25 -0800

    Made RetailSales a functional comp

--- src/components/RetailSales.js
+++ src/components/RetailSales.js
@@ -3,19 +3,17 @@ import React, { Component } from 'react';
 import SalesChart from '../containers/SalesChart';
 import SalesTable from '../containers/SalesTable';
 
-class RetailSales extends Component {
-    render() {
-        return (
-            <div className="col-md-9" id="retail_sales">
-                <div className="container">
-                    <div className="row">
-                        <SalesChart />
-                        <SalesTable />
-                    </div>
+const RetailSales = () => {
+    return (
+        <div className="col-md-9" id="retail_sales">
+            <div className="container">
+                <div className="row">
+                    <SalesChart />
+                    <SalesTable />
                 </div>
             </div>
-        )
-    }
-}
+        </div>
+    )
+};
 
 export default RetailSales;
